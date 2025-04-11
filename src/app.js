098e277c2b2c05d55cdd77dba1c28f440b854711
SANTIAGO VARELA <48245881@ort>
2025-04-11 10:41:26 -0300

    3

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) =>  {
+    res.send('Bienvenido a mi servidor');
+})
+app.get('/hora', (req, res) =>  {
+    res.send();
+})
+app.get('/fecha-completa', (req, res) =>  {
+    res.send();
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
+})
